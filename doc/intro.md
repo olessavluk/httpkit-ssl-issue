e4b6336945e0b2611992852a3d7f6dbf7b77a7b1
Oles Savluk <savluk.oles@gmail.com>
2017-04-22 23:26:58 +0300

    app template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to httpkit-ssl-issue
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/httpkit_ssl_issue/core_test.clj
@@ -0,0 +1,7 @@
+(ns httpkit-ssl-issue.core-test
+  (:require [clojure.test :refer :all]
+            [httpkit-ssl-issue.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
